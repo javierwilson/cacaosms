2da2ba606466771eb911d9863e78ecdf8e7736f7
Juan Carlos Álvarez Vélez <alvarezvelez.juancarlos@gmail.com>
2016-11-28 03:02:17 -0600

    agregando campos faltantes

--- cacaosms/admin.py
+++ cacaosms/admin.py
@@ -7,45 +7,7 @@ class ContactoInline(admin.TabularInline):
 
 
 
-admin.site.register(Mensaje)
-
-
-
-
-admin.site.register(Bitacora)
-
-
-
-
-admin.site.register(Respuesta)
-
-
-
-
-admin.site.register(Trivia)
-
-
-
-
 admin.site.register(ContactoTipo)
-
-
-
-
-admin.site.register(Envios)
-
-
-
-
-class ContactoAdmin(admin.ModelAdmin):
-    list_display = ['contactotipo','nombre','telefono',]
-    list_filter = ('contactotipo',)
-    search_fields = ['nombre','telefono',]
-    
-    list_per_page = 100
-    
-
-admin.site.register(Contacto, ContactoAdmin)
 
 
 
@@ -65,6 +27,103 @@ class PaisAdmin(admin.ModelAdmin):
     
 
 admin.site.register(Pais, PaisAdmin)
+
+
+
+
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ['nombre','telefono','pais','contactotipo','grupo',]
+    list_filter = ('pais','contactotipo','grupo',)
+    search_fields = ['nombre','telefono',]
+    
+    list_per_page = 100
+    
+
+admin.site.register(Contacto, ContactoAdmin)
+
+
+
+
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = ['nombre',]
+    list_filter = ('nombre',)
+    
+    
+    
+    
+    inlines = [
+    
+        ContactoInline,
+    
+    ]
+    
+
+admin.site.register(Grupo, GrupoAdmin)
+
+
+
+
+class BitacoraAdmin(admin.ModelAdmin):
+    list_display = ['de','para','mensaje','fecha',]
+    list_filter = ('de','para','fecha',)
+    
+    
+    
+    
+
+admin.site.register(Bitacora, BitacoraAdmin)
+
+
+
+
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ['nombre','mensaje',]
+    
+    
+    
+    
+    
+
+admin.site.register(Mensaje, MensajeAdmin)
+
+
+
+
+class RespuestaAdmin(admin.ModelAdmin):
+    list_display = ['nombre','mensaje',]
+    
+    
+    
+    
+    
+
+admin.site.register(Respuesta, RespuestaAdmin)
+
+
+
+
+class TriviaAdmin(admin.ModelAdmin):
+    list_display = ['nombre',]
+    
+    
+    
+    
+    
+
+admin.site.register(Trivia, TriviaAdmin)
+
+
+
+
+class EnviosAdmin(admin.ModelAdmin):
+    list_display = ['de','texto','mensaje',]
+    list_filter = ('de',)
+    
+    
+    
+    
+
+admin.site.register(Envios, EnviosAdmin)
 
 
 
--- cacaosms/models.py
+++ cacaosms/models.py
@@ -5,58 +5,6 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext as _
 
 
-
-class Mensaje(models.Model):
-
-    
-
-    class Meta:
-        ordering = []
-        verbose_name = _(u'Mensaje')
-        verbose_name_plural = _(u'Mensaje')
-
-    
-
-
-
-class Bitacora(models.Model):
-
-    
-
-    class Meta:
-        ordering = []
-        verbose_name = _(u'Bitacora')
-        verbose_name_plural = _(u'Bitacora')
-
-    
-
-
-
-class Respuesta(models.Model):
-
-    
-
-    class Meta:
-        ordering = []
-        verbose_name = _(u'Respuesta')
-        verbose_name_plural = _(u'Respuesta')
-
-    
-
-
-
-class Trivia(models.Model):
-
-    
-
-    class Meta:
-        ordering = []
-        verbose_name = _(u'Trivia')
-        verbose_name_plural = _(u'Trivia')
-
-    
-
-
 @python_2_unicode_compatible
 class ContactoTipo(models.Model):
 
@@ -71,39 +19,6 @@ class ContactoTipo(models.Model):
     
     def __str__(self):
         return "%s " % (self.nombre, )
-    
-
-
-
-class Envios(models.Model):
-
-    
-
-    class Meta:
-        ordering = []
-        verbose_name = _(u'Envios')
-        verbose_name_plural = _(u'Envios')
-
-    
-
-
-@python_2_unicode_compatible
-class Contacto(models.Model):
-
-    pais = models.ForeignKey('Pais', verbose_name=_(u'País'))
-    contactotipo = models.ForeignKey('ContactoTipo', verbose_name=_(u'Tipo'))
-    nombre = models.CharField(max_length=200, verbose_name=_(u'Nombre'))
-    telefono = models.IntegerField(verbose_name=_(u'Teléfono'))
-    
-
-    class Meta:
-        ordering = ['nombre',]
-        verbose_name = _(u'Contacto')
-        verbose_name_plural = _(u'Contacto')
-
-    
-    def __str__(self):
-        return "%s %s " % (self.nombre, self.telefono, )
     
 
 
@@ -122,6 +37,140 @@ class Pais(models.Model):
     
     def __str__(self):
         return "%s " % (self.nombre, )
+    
+
+
+@python_2_unicode_compatible
+class Contacto(models.Model):
+
+    nombre = models.CharField(max_length=200, verbose_name=_(u'Nombre'))
+    telefono = models.IntegerField(verbose_name=_(u'Teléfono'))
+    pais = models.ForeignKey('Pais', verbose_name=_(u'País'))
+    contactotipo = models.ForeignKey('ContactoTipo', null=True, blank=True, verbose_name=_(u'Tipo'))
+    grupo = models.ForeignKey('Grupo', null=True, blank=True, verbose_name=_(u'Grupo'))
+    
+
+    class Meta:
+        ordering = ['nombre',]
+        verbose_name = _(u'Contacto')
+        verbose_name_plural = _(u'Contacto')
+
+    
+    def __str__(self):
+        return "%s %s " % (self.nombre, self.telefono, )
+    
+
+
+@python_2_unicode_compatible
+class Grupo(models.Model):
+
+    nombre = models.CharField(max_length=100, verbose_name=_(u'Nombre'))
+    
+
+    class Meta:
+        ordering = ['nombre',]
+        verbose_name = _(u'Agrupación')
+        verbose_name_plural = _(u'Agrupación')
+
+    
+    def __str__(self):
+        return "%s " % (self.nombre, )
+    
+
+
+@python_2_unicode_compatible
+class Bitacora(models.Model):
+
+    de = models.IntegerField(verbose_name=_(u''))
+    para = models.IntegerField(verbose_name=_(u''))
+    mensaje = models.CharField(max_length=160, verbose_name=_(u''))
+    fecha = models.DateTimeField(verbose_name=_(u''))
+    
+
+    class Meta:
+        ordering = []
+        verbose_name = _(u'Bitacora')
+        verbose_name_plural = _(u'Bitacora')
+
+    
+    def __str__(self):
+        return "%s %s %s " % (self.de, self.para, self.fecha, )
+    
+
+
+@python_2_unicode_compatible
+class Mensaje(models.Model):
+
+    nombre = models.CharField(max_length=100, verbose_name=_(u''))
+    mensaje = models.CharField(max_length=160, verbose_name=_(u''))
+    
+
+    class Meta:
+        ordering = ['nombre',]
+        verbose_name = _(u'Mensaje')
+        verbose_name_plural = _(u'Mensaje')
+
+    
+    def __str__(self):
+        return "%s " % (self.nombre, )
+    
+
+
+@python_2_unicode_compatible
+class Respuesta(models.Model):
+
+    nombre = models.CharField(max_length=160, verbose_name=_(u''))
+    mensaje = models.CharField(max_length=160, verbose_name=_(u'Mensaje de respuesta'))
+    
+
+    class Meta:
+        ordering = ['nombre',]
+        verbose_name = _(u'Respuesta')
+        verbose_name_plural = _(u'Respuesta')
+
+    
+    def __str__(self):
+        return "%s " % (self.nombre, )
+    
+
+
+@python_2_unicode_compatible
+class Trivia(models.Model):
+
+    nombre = models.CharField(max_length=160, verbose_name=_(u''))
+    
+
+    class Meta:
+        ordering = ['nombre',]
+        verbose_name = _(u'Trivia')
+        verbose_name_plural = _(u'Trivia')
+
+    
+    def __str__(self):
+        return "%s " % (self.nombre, )
+    
+
+
+@python_2_unicode_compatible
+class Envios(models.Model):
+
+    de = models.CharField(max_length=100, verbose_name=_(u'Quién envía'))
+    para_pais = models.ForeignKey('Pais', null=True, blank=True, verbose_name=_(u'Para todo un país'))
+    para_contactotipo = models.ForeignKey('ContactoTipo', null=True, blank=True, verbose_name=_(u'Para todo un tipo de contacto'))
+    para_contacto = models.ForeignKey('Contacto', null=True, blank=True, verbose_name=_(u'Para un número específico'))
+    para_grupo = models.ForeignKey('Grupo', null=True, blank=True, verbose_name=_(u'Para un grupo'))
+    texto = models.CharField(max_length=160, null=True, blank=True, verbose_name=_(u'Mensaje personalizado'))
+    mensaje = models.ForeignKey('Mensaje', null=True, blank=True, verbose_name=_(u'Mensaje'))
+    
+
+    class Meta:
+        ordering = []
+        verbose_name = _(u'Envíos')
+        verbose_name_plural = _(u'Envíos')
+
+    
+    def __str__(self):
+        return "%s " % (self.de, )
     
 
 
