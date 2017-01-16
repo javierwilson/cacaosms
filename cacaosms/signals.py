20863b4fd1d2b6eebec1793d10660387a9c02010
Juan Carlos Álvarez Vélez <alvarezvelez.juancarlos@gmail.com>
2017-01-16 20:14:08 +0000

    pais y grupo

--- cacaosms/signals.py
+++ cacaosms/signals.py
@@ -7,10 +7,13 @@ from cacaosms.models import Envios, Contacto
 
 @receiver(post_save, sender=Envios, dispatch_uid="send_sms_to_queue")
 def send_sms_to_queue(sender, instance, **kwargs):
-    print instance.para_pais
     if instance.para_pais:
         contacts = Contacto.objects.filter(pais=instance.para_pais)
-        print contacts
+        for contact in contacts:
+            print "%s: %s" % (contact.full_number, instance.message)
+            send_sms(contact.full_number, instance.message)
+    if instance.para_grupo:
+        contacts = Contacto.objects.filter(grupo=instance.para_grupo)
         for contact in contacts:
             print "%s: %s" % (contact.full_number, instance.message)
             send_sms(contact.full_number, instance.message)
