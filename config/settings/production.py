7486a92a224c8ff0a811dea2afb79db5aa2f6aee
Javier Wilson <javier.wilson@gmail.com>
2017-06-29 10:48:43 +0000

    add raven

--- config/settings/production.py
+++ config/settings/production.py
@@ -209,3 +209,5 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
+RAVEN_CONFIG = {'dsn': env('DJANGO_RAVEN_DSN')}
